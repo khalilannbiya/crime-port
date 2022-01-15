084a62508fd17bc2a9550480443ff53b341e1f63
khalilannbiya <syeichkhalil30@gmail.com>
2022-01-15 08:45:15 +0700

    update login page

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -23,36 +23,38 @@
                 <img src="<?= base_url('assets/image/logo.svg'); ?>" alt="" class="logo">
                 <p class="crime">E-CrimePort</p>
             </div>
-            <div class="polisi">
-                <img src="<?= base_url('assets/image/motif-geometris.svg'); ?>" alt="" class="motif">
-                <img class="police" src="<?= base_url('assets/image/polisi.svg'); ?>" alt="">
+            <div class="wrapper__polisi">
+                <div class="polisi">
+                    <img src="<?= base_url('assets/image/motif-geometris.svg'); ?>" alt="" class="motif">
+                    <img class="police" src="<?= base_url('assets/image/polisi.svg'); ?>" alt="">
+                </div>
+                <p class="moto">Report The Crimes You Have <br>Experienced</p>
             </div>
-            <p class="moto">Report The Crimes You Have <br>Experienced</p>
         </div>
         <div class="setengah kanan">
-            <!-- <img src="<?= base_url('assets/image/motif-ombak.svg'); ?>" alt="" class="motif-om"> -->
-            <div class="sambutan">
-                <p class="welcome">Welcome To E-CrimePort</p>
-                <p class="text-kecil">Belum Punya Akun? <a href="<?= base_url('auth/registrasi'); ?>">Sign Up</a></p>
+            <div>
+                <!-- <img src="<?= base_url('assets/image/motif-ombak.svg'); ?>" alt="" class="motif-om"> -->
+                <div class="sambutan">
+                    <p class="welcome">Welcome To E-CrimePort</p>
+                    <p class="text-kecil">Belum Punya Akun? <a href="<?= base_url('auth/registrasi'); ?>">Sign Up</a></p>
+                </div>
+                <div class="pesan-logout"> <?= $this->session->flashdata('message'); ?></div>
+                <form class="form-absolute" method="POST" action="<?= base_url('auth'); ?>">
+                    <div class="email">
+                        <label for="email">Email</label>
+                        <input type="text" id="email" name="email" placeholder="Email"><br>
+                        <p class="form-error"><?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?></p>
+                    </div>
+                    <div class="pw">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" placeholder="Password">
+                        <p class="form-error"><?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?></p>
+                    </div>
+                    <button class="btn btn-primary " type="submit">
+                        Sign In
+                    </button>
+                </form>
             </div>
-            <div class="pesan-logout"> <?= $this->session->flashdata('message'); ?></div>
-            <form class="form-absolute" method="POST" action="<?= base_url('auth'); ?>">
-                <div class="email">
-                    <label for="email">Email</label>
-                    <input type="text" id="email" name="email" placeholder="Email"><br>
-                    <p class="form-error"><?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?></p>
-                </div>
-                <div class="pw">
-                    <label for="password">Password</label>
-                    <input type="password" id="password" name="password" placeholder="Password">
-                    <p class="form-error"><?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?></p>
-                </div>
-                <button class="btn btn-primary " type="submit">
-                    Login
-                </button>
-            </form>
-
-
         </div>
     </div>
     <!-- Optional JavaScript; choose one of the two! -->
@@ -60,13 +62,6 @@
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
-    -->
 </body>
 
 </html>
